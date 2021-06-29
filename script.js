7bc5fae6c32ee0e51009ed570d40789c75186e1a
Nícholas Eras <nicoeras@hotmail.com>
2021-06-29 18:54:52 -0300

    v10.0
    PHP

---
+++ script.js
@@ -0,0 +1,41 @@
+
+function stop(){
+  document.querySelector("video").pause();
+  }
+  
+  function modal(){
+      document.querySelector(".modal").style.visibility = 'visible';
+      document.querySelector(".baixo").style.visibility = "hidden";
+  }
+  
+  function modalpd(){
+      document.querySelector(".modalpd").style.visibility = 'visible';
+      document.querySelector(".baixo").style.visibility = "hidden";
+  }
+  function modalpgi(){
+      document.querySelector(".modalpgi").style.visibility = 'visible';
+      document.querySelector(".baixo").style.visibility = "hidden";
+  }
+  function modaljs(){
+      document.querySelector(".modaljs").style.visibility = 'visible';
+      document.querySelector(".baixo").style.visibility = "hidden";
+  }
+  function modalpyf(){
+      document.querySelector(".modalpyf").style.visibility = 'visible';
+      document.querySelector(".baixo").style.visibility = "hidden";
+  }
+  function modalphp(){
+      document.querySelector(".modalphp").style.visibility = 'visible';
+      document.querySelector(".baixo").style.visibility = "hidden";
+  }
+  function closemo(){
+      document.querySelector(".modal").style.visibility = 'hidden';
+      document.querySelector(".modaljs").style.visibility = 'hidden';
+      document.querySelector(".modalpgi").style.visibility = 'hidden';
+      document.querySelector(".modalpd").style.visibility = 'hidden';
+      document.querySelector(".modalpyf").style.visibility = 'hidden';
+      document.querySelector(".modalphp").style.visibility = 'hidden';
+      document.querySelector(".baixo").style.boxShadow = "0px 0px 5px";
+      document.querySelector(".baixo").style.visibility = "visible";
+  }
+  
